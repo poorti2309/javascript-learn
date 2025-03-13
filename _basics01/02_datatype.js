0f76ef3da2be45bf7527b267a3c84001c647280e
poorti2309 <gpoorti2309@gmail.com>
2025-03-13 10:37:14 +0000

    datatypes discussed

---
+++ _basics01/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3+3 ) We are using nodejs not browser that's why it can't provide pop up
+let Name = "Poorti"
+let num = 3
+let isLoggedIn = true
+
+//number(int) => Range(up to 2^53)
+//string => ""
+//boolean => true/false
+//null => standalone value 
+//undefined =>
+// symbol => used for uniqueness one
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
