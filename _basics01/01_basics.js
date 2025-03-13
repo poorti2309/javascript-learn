7982777b176e4eb3ff16daf282219b9fddda22a2
poorti2309 <gpoorti2309@gmail.com>
2025-03-13 10:37:59 +0000

    basucs file added

---
+++ _basics01/01_basics.js
@@ -0,0 +1,17 @@
+const accountId = 185247
+let accountEmail = "poorti@google.com"
+var accountName = "Poorti2309"
+
+accountCity = "Jaipur" //We can declare variables without data type also but it not an good practice
+
+let accountState;  // if we declare an variable without assigning value then it will store value as undefined.
+
+// accountId=2  ----> Const one can't be change after one declaration
+
+// To print too many console statements we can use console.table([With commas write what you wnat to print])
+
+/*Ignore using var datatype
+Because of block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountName, accountCity, accountState])
