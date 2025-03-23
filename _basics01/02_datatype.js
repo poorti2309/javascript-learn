612e6196070994ff6569edbae633fcf3652a3d10
poorti2309 <gpoorti2309@gmail.com>
2025-03-23 15:36:37 +0530

    modified

--- _basics01/02_datatype.js
+++ _basics01/02_datatype.js
@@ -1,6 +1,6 @@
 "use strict"; //treat all JS code as newer version
 
-// alert( 3+3 ) We are using nodejs not browser that's why it can't provide pop up
+// alert( 3+3 ) We are using nodejs not browser's console that's why it can't provide pop up
 let Name = "Poorti"
 let num = 3
 let isLoggedIn = true
