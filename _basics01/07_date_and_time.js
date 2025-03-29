1ec399ff0d4b706c2d28af3de1b0df2c9848e51d
poorti2309 <gpoorti2309@gmail.com>
2025-03-29 22:40:00 +0530

    Array methods and all updated

---
+++ _basics01/07_date_and_time.js
@@ -0,0 +1,16 @@
+// Dates
+
+let myDate = new Date
+ let new_Date = myDate.toString()
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toUTCString());
+// console.log(typeof myDate);
+// console.log(typeof new_Date)
+
+let mycreated_Date = new Date(2023, 3, 23)
+console.log(mycreated_Date.toDateString())
+
