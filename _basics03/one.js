da89848dbb606c2b134f72e76f25e280bbb67ec2
poorti2309 <gpoorti2309@gmail.com>
2025-04-01 15:27:23 +0530

    Control flow

---
+++ _basics03/one.js
@@ -0,0 +1,27 @@
+const isUserloggedIn = true;
+
+//Operators ----> < , >, <=, >=, ==, !=
+
+// if(2 === "2"){
+//       console.log("Executed");
+// }
+
+//Var ---> global Scope ki tarah hi behave krta hai that'why we use let and const always
+
+// let score = 200
+
+// if(score > 100){
+//        var power = "fly"
+//     // const power = "fly"
+//     console.log(`User Power: ${power}`)
+// }
+
+// console.log(`User power:${power}`);
+const balance = 1000
+   // Wrong Practice to use if statement
+// if (balance > 500) console.log(test),console.log("test2");
+
+//If else and switch statemnt
+
+
+
