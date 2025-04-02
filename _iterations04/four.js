a71f7822c46351f03121d1aeab09994cb2120ff8
poorti2309 <gpoorti2309@gmail.com>
2025-04-02 15:12:32 +0530

    Added new file and Apply ForEach loop on Maps

--- _iterations04/four.js
+++ _iterations04/four.js
@@ -33,7 +33,19 @@
   ]
 
   myCoding.forEach((item)=>{
-    console.log(item.languageFileName);
+    //console.log(item.languageFileName);
+  })
+
+  // ForEach can also used to make map iterable
+
+  const map = new Map()
+  map.set('IN', "India")
+  map.set('USA', "Unites States of America")
+  map.set('Fr', "France")
+  map.set('In', "India")
+
+  map.forEach((  value, key) => {
+    console.log(key, value);
   })
 
 
